b6c8c280716ac08bd666a8eb16d4298cf9251e8d
Ayush Singh <112885157+Ayush200Singh@users.noreply.github.com>
2022-09-29 12:45:25 +0530

    Add files via upload

---
+++ Abbstarction.java
@@ -0,0 +1,33 @@
+package corejava;
+abstract class Car{
+	void car() {
+		System.out.println("Car is built in");
+	}
+	abstract void Drive();
+}
+class Tesla extends Car{
+
+	@Override
+	void Drive() {
+		System.out.println("Drive Safely");
+		
+	}
+	void GearChange() {
+		System.out.println("Gear Changed");
+		
+	}
+	void Speed() {
+		System.out.println("Increase speed");
+	}
+	
+}
+public class Abstraction {
+	public static void main(String[] args) {
+	 Tesla ts = new Tesla();
+	 ts.car();
+	 ts.Drive();
+	 ts.GearChange();
+	 ts.Speed();
+	}
+
+}
